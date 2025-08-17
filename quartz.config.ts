cbb670309d72eb8d478271d5830f41c330dc8983
Shea Brown <shea@sublimeplants.com>
2025-08-17 16:38:10 -0500

    Small tweaks
    Added title, and test page because it's getting served wonky

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Sublime Plants Docs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
